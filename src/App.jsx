61f84e820d62b08b3013aebe6dc4cdd1cee2d8fa
Andrew Christie <awchristie86@gmail.com>
2022-02-04 19:30:09 -0500

    doesn't work :(

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import web3 from './web3';
+
+function App() {
+  // console.log(web3.version)
+  return <h1>Yo!</h1>
+}
+
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+// import web3 from './web3';
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
